9a416f2f3f9260f29d8d0c6bd1192e20b0c6957f
Dr0wnP00l <of4hrsmith@gmail.com>
2020-09-21 12:02:11 -0700

    Stuff no worky

--- Commands/avatar.js
+++ Commands/avatar.js
@@ -1,16 +1,19 @@
 module.exports = {
 	name: 'avatar',
-	description: 'Get the avatar URL of the tagged user(s), or your own avatar.',
-	aliases: ['icon', 'pfp'],
-	execute(message) {
+	aliases: ['icon', 'logo'],
+	description: 'display the image and url of users\' avatar',
+	usage: '[tagged users]',
+	execute(client, kayn, REGIONS, config, message) {
 		if (!message.mentions.users.size) {
-			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL}>`);
+			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
 		}
 
 		const avatarList = message.mentions.users.map(user => {
-			return `${user.username}'s avatar: <${user.displayAvatarURL}>`;
+			return `${user.username}'s avatar: ${user.displayAvatarURL}`;
 		});
 
+		// send the entire array of strings as a message
+		// by default, discord.js will `.join()` the array with `\n`
 		message.channel.send(avatarList);
 	},
 };
